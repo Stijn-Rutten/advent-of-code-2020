c99e405475e4cbff9656697c27bd33ac8c603a35
Stijn Rutten <stijn7rutten@hotmail.com>
2020-12-02 23:23:40 +0100

    add prettier and eslint

--- src/day-2/password-validator.ts
+++ src/day-2/password-validator.ts
@@ -2,82 +2,99 @@ import { match } from 'assert';
 import fs from 'fs';
 
 interface PasswordConfig {
-    requiredCharacter: {
-        character: string,
-        requirementOne: number,
-        requirementTwo: number
-    };
-    password: string;
+  requiredCharacter: {
+    character: string;
+    requirementOne: number;
+    requirementTwo: number;
+  };
+  password: string;
 }
 
 export class PasswordValidator {
-    private readonly _file: string;
-    private get _passwordConfigs(): PasswordConfig[] {
-        return this._file.trim().split(/\n/g).map(x => this._convertStringToPasswordConfig(x)) as PasswordConfig[];
-    }
-    constructor() {
-        this._file = fs.readFileSync('./src/day-2/assets/passwords.txt').toString();
-    }
+  private readonly _file: string;
+  private get _passwordConfigs(): PasswordConfig[] {
+    return this._file
+      .trim()
+      .split(/\n/g)
+      .map((x) => this._convertStringToPasswordConfig(x)) as PasswordConfig[];
+  }
+  constructor() {
+    this._file = fs.readFileSync('./src/day-2/assets/passwords.txt').toString();
+  }
 
-    getNoOfValidPasswordsByCharacterCount(): number {
-        let validPasswords = 0;
+  getNoOfValidPasswordsByCharacterCount(): number {
+    let validPasswords = 0;
 
-        for (const passwordConfig of this._passwordConfigs) {
-            const letterCount = this._getCharacterCountInPassword(passwordConfig.password, passwordConfig.requiredCharacter.character);
-            if (letterCount >= passwordConfig.requiredCharacter.requirementOne && letterCount <= passwordConfig.requiredCharacter.requirementTwo) {
-                validPasswords++;
-            }
-        }
-
-        return validPasswords;
-    }
-
-    getNoOfValidPasswordsByCharacterIndex(): number {
-        let validPasswords = 0;
-
-        for (const passwordConfig of this._passwordConfigs) {
-            
-            const requiredCharacter = passwordConfig.requiredCharacter;
-            const matchFirstRequirement = passwordConfig.password.charAt(requiredCharacter.requirementOne - 1) === requiredCharacter.character;
-            const matchSecondRequirement = passwordConfig.password.charAt(requiredCharacter.requirementTwo - 1) === requiredCharacter.character;
-
-            if ((matchFirstRequirement || matchSecondRequirement) && (matchFirstRequirement !== matchSecondRequirement)) {
-                validPasswords++;
-            }
-        }
-
-        return validPasswords;
+    for (const passwordConfig of this._passwordConfigs) {
+      const letterCount = this._getCharacterCountInPassword(
+        passwordConfig.password,
+        passwordConfig.requiredCharacter.character
+      );
+      if (
+        letterCount >= passwordConfig.requiredCharacter.requirementOne &&
+        letterCount <= passwordConfig.requiredCharacter.requirementTwo
+      ) {
+        validPasswords++;
+      }
     }
 
+    return validPasswords;
+  }
 
+  getNoOfValidPasswordsByCharacterIndex(): number {
+    let validPasswords = 0;
 
-    private _getCharacterCountInPassword(password: string, character: string): number {
-        let letterCount = 0;
-        for (let i = 0; i < password.length; i++) {
-            if (password.charAt(i) === character) {
-                letterCount++;
-            }
-        }
+    for (const passwordConfig of this._passwordConfigs) {
+      const requiredCharacter = passwordConfig.requiredCharacter;
+      const matchFirstRequirement =
+        passwordConfig.password.charAt(requiredCharacter.requirementOne - 1) ===
+        requiredCharacter.character;
+      const matchSecondRequirement =
+        passwordConfig.password.charAt(requiredCharacter.requirementTwo - 1) ===
+        requiredCharacter.character;
 
-        return letterCount;
+      if (
+        (matchFirstRequirement || matchSecondRequirement) &&
+        matchFirstRequirement !== matchSecondRequirement
+      ) {
+        validPasswords++;
+      }
     }
 
-    private _convertStringToPasswordConfig(passwordString: string): PasswordConfig | undefined {
-        const regex = /(\d+)-(\d+)\W(\w):\W(\w+)/;
+    return validPasswords;
+  }
 
-        var match = regex.exec(passwordString);
-        if (match) {
-            return {
-                requiredCharacter: {
-                    requirementOne: +match[1],
-                    requirementTwo: +match[2],
-                    character: match[3]
-
-                },
-                password: match[4]
-            }
-        }
-
-        return undefined;
+  private _getCharacterCountInPassword(
+    password: string,
+    character: string
+  ): number {
+    let letterCount = 0;
+    for (let i = 0; i < password.length; i++) {
+      if (password.charAt(i) === character) {
+        letterCount++;
+      }
     }
+
+    return letterCount;
+  }
+
+  private _convertStringToPasswordConfig(
+    passwordString: string
+  ): PasswordConfig | undefined {
+    const regex = /(\d+)-(\d+)\W(\w):\W(\w+)/;
+
+    var match = regex.exec(passwordString);
+    if (match) {
+      return {
+        requiredCharacter: {
+          requirementOne: +match[1],
+          requirementTwo: +match[2],
+          character: match[3]
+        },
+        password: match[4]
+      };
+    }
+
+    return undefined;
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
-import { ReportRepair } from "./day-1/report-repair";
-import { PasswordValidator } from "./day-2/password-validator";
-
+import { ReportRepair } from './day-1/report-repair';
+import { PasswordValidator } from './day-2/password-validator';
 
 /// Day 1:
 const reportRepair = new ReportRepair();
-console.log("Solution day 1-1:", reportRepair.findTheTwoNumbers());
-console.log("Solution day 1-2:", reportRepair.findTheThreeNumbers());
-
+console.log('Solution day 1-1:', reportRepair.findTheTwoNumbers());
+console.log('Solution day 1-2:', reportRepair.findTheThreeNumbers());
 
 /// Day 2:
 const passwordValidator = new PasswordValidator();
-console.log("Solution day 2-1:", passwordValidator.getNoOfValidPasswordsByCharacterCount());
-console.log("Solution day 2-2:", passwordValidator.getNoOfValidPasswordsByCharacterIndex());
+console.log(
+  'Solution day 2-1:',
+  passwordValidator.getNoOfValidPasswordsByCharacterCount()
+);
+console.log(
+  'Solution day 2-2:',
+  passwordValidator.getNoOfValidPasswordsByCharacterIndex()
+);
